f4f5b20b2f43cd8e4bed78db2ec5b6d0179589b3
Martin Kubus <martin.kubus@disig.sk>
2019-11-12 10:53:20 +0100

    Add project files.

---
+++ Training/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime timeUtc = DateTime.UtcNow;
+           var x = timeUtc.ToLocalTime();
+
+            int? a = 1;
+
+            var e = a.HasValue;
+
+            try
+            {
+                TimeZoneInfo cstZone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+                DateTime cstTime = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, cstZone);
+                Console.WriteLine("The date and time are {0} {1}.",
+                                  cstTime,
+                                  cstZone.IsDaylightSavingTime(cstTime) ?
+                                          cstZone.DaylightName : cstZone.StandardName);
+            }
+            catch (TimeZoneNotFoundException)
+            {
+                Console.WriteLine("The registry does not define the Central Standard Time zone.");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
